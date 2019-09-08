31e82efcad105f82f9a1c27a79f17678e004a9df
Shahed <shahed739@gmail.com>
2019-09-09 03:10:32 +0600

    login service with token generation

--- project/server/__init__.py
+++ project/server/__init__.py
@@ -3,8 +3,10 @@
 import os
 from project.server.auth.views import auth_blueprint
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app_settings = os.getenv(
     'APP_SETTINGS',
--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -1,24 +1,104 @@
-from flask import Blueprint, request, make_response, jsonify
+from flask import Blueprint, make_response, jsonify
+import requests
+import json
+import pprint
 from flask.views import MethodView
 
 auth_blueprint = Blueprint('auth', __name__)
 
 
-class TestApi(MethodView):
+class LoginApi(MethodView):
     """
-    This is a test API
+    Login API to port : 5000
     """
+    base_url = 'http://cloud.brilliant.com.bd:5000/identity/v3/'
+    global tokens
+    tokens = {
+        'responseStatus': '',
+        'unScopedToken': '',
+        'scopedToken': '',
+        'projectID': ''
+    }
+
     def get(self):
+        tokenData = self.fetchUnscopedToken(self.base_url)
+        if tokens['responseStatus'] == 'ok':
+            LoginApi.fetchOwnProjects(self.base_url, tokenData, tokens['unScopedToken'])
         responseObject = {
             'status': 'success',
-            'message': 'This is a successful call'
+            'message': 'This is a successful call',
+            'unScopedToken': tokens['unScopedToken'],
+            'scopedToken': tokens['scopedToken']
         }
-        return make_response(jsonify(responseObject)), 200
+        if tokens['responseStatus'] == 'ok':
+            return make_response(jsonify(responseObject)), 200
+        return make_response(jsonify({'error': 'There has been a problem. Please try again!'}))
+
+    @staticmethod
+    def fetchUnscopedToken(url):
+        auth = {
+            'auth': {
+                'identity': {
+                    'methods': ['password'],
+                    'password': {
+                        'user': {
+                            'name': 'shajal',
+                            'domain': {
+                                'name': 'Default'
+                            },
+                            'password': 'Sh0!l-9Ep3lk#1'
+                        },
+
+                    }
+                }
+            }
+        }
+        response = requests.post(url+'auth/tokens', data=json.dumps(auth),
+            headers={'Content-type': 'application/json'})
+        tokens['responseStatus'] = 'ok' if response.status_code == 201 else 'failed'
+        if tokens['responseStatus'] != 'ok':
+            return
+        tokens['unScopedToken'] = response.headers['X-Subject-Token']
+        return response.json()
+
+    @staticmethod
+    def fetchOwnProjects(url, data, token):
+        userID = data['token']['user']['id']
+        header = {
+            'X-Auth-Token': token
+        }
+        response = requests.get(url+'users/'+userID+'/projects', headers=header)
+        tokens['responseStatus'] = 'ok' if response.status_code == 200 else 'failed'
+        responseData = response.json()
+        data = responseData['projects'][0]['id']
+        LoginApi.fetchScopedToken(url, data, token)
+
+    @staticmethod
+    def fetchScopedToken(url, data, token):
+        auth = {
+            'auth': {
+                'identity': {
+                    'methods': ['token'],
+                    'token': {
+                        'id': token
+                    }
+                },
+                'scope': {
+                    'project': {
+                        'id': data
+                    }
+                }
+            }
+        }
+        response = requests.post(url+'auth/tokens', data=json.dumps(auth),
+            headers={'Content-type': 'application/json'})
+        tokens['projectID'] = data
+        tokens['scopedToken'] = response.headers['X-Subject-Token']
 
 
-testApi_view = TestApi.as_view('test_api')
+LoginApi_view = LoginApi.as_view('login_api')
 auth_blueprint.add_url_rule(
-    '/test',
-    view_func=testApi_view,
+    '/login',
+    view_func=LoginApi_view,
     methods=['GET']
     )
